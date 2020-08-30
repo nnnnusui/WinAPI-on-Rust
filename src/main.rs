69565b1b4ecf695f6256a23dd5dba0724f10d02e
nnnnusui <nnnnusui@gmail.com>
2020-08-30 16:03:23 +0900

    Display MessageBox
    reference: https://qiita.com/benki/items/778ba2bd1266d28bce61

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use winapi::um::winuser::{MessageBoxW, MB_OK};
+use std::ptr;
+
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        MessageBoxW(
+            ptr::null_mut(), //hWnd: HWND
+            encode("Hello, World!").as_ptr(), //lpText: LPCWSTR
+            encode("こんにちわ、世界！").as_ptr(), //lpCaption: LPCWSTR
+            MB_OK); //uType: UINT
+    }
+}
+
+fn encode(source: &str) -> Vec<u16> {
+    source.encode_utf16().chain(Some(0)).collect()
 }
