5c31c65ad823ea8d745c79203307615fcf8a1518
nnnnusui <nnnnusui@gmail.com>
2020-08-30 20:36:23 +0900

    monitor listup sample

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,86 @@
+use std::io::Error;
+use std::mem;
+use std::ptr;
+
 use winapi::{
-    um::{
-        winuser::{RegisterClassW, WNDCLASSW, CS_HREDRAW, CS_VREDRAW,
-                  LoadIconW, IDI_APPLICATION, LoadCursorW, IDC_ARROW,
-                  CreateWindowExW, ShowWindow, SW_NORMAL, UpdateWindow,
-                  GetMessageW, TranslateMessage, DispatchMessageW, MSG,
-                  WM_DESTROY, PostQuitMessage, DefWindowProcW, WS_OVERLAPPEDWINDOW},
-        wingdi::{GetStockObject, WHITE_BRUSH},
-    },
     shared::{
-        windef::{HWND, HBRUSH},
-        minwindef::{UINT, WPARAM, LPARAM, LRESULT},
+        minwindef::{BOOL, LPARAM, LRESULT, TRUE, UINT, WPARAM},
+        windef::{HBRUSH, HDC, HMONITOR, HWND, LPRECT, RECT},
+    },
+    um::{
+        wingdi::{GetStockObject, WHITE_BRUSH},
+        winuser::{CreateWindowExW, CS_HREDRAW, CS_VREDRAW, DefWindowProcW,
+                  DispatchMessageW, GetMessageW, IDC_ARROW, IDI_APPLICATION,
+                  LoadCursorW, LoadIconW, MSG, PostQuitMessage,
+                  RegisterClassW, ShowWindow, SW_NORMAL, TranslateMessage,
+                  UpdateWindow, WM_DESTROY, WNDCLASSW, WS_OVERLAPPEDWINDOW},
     },
 };
-use std::ptr;
-use std::mem;
-use winapi::um::winuser::CW_USEDEFAULT;
+use winapi::um::winuser::{CW_USEDEFAULT, EnumDisplayMonitors, GetMonitorInfoW, MonitorFromPoint, MONITORINFOEXW, SW_SHOWNORMAL};
 
 fn main() {
     unsafe {
-        let class_name = encode("my_window_class_name");
-        if !register_wndclass(&class_name) {
-            return;
-        }
+        for monitor in enumerate_monitors() {
+            // Convert the WCHAR[] to a unicode OsString
+            use std::ffi::OsString;
+            use std::os::windows::ffi::OsStringExt;
+            let name = match &monitor.szDevice[..].iter().position(|c| *c == 0) {
+                Some(len) => OsString::from_wide(&monitor.szDevice[0..*len]),
+                None => OsString::from_wide(&monitor.szDevice[0..monitor.szDevice.len()]),
+            };
 
-        let hwnd = create_window(&class_name);
-        if hwnd.is_null() {
-            return;
-        }
-        ShowWindow(hwnd, SW_NORMAL);
-        UpdateWindow(hwnd);
-        let mut msg = mem::uninitialized::<MSG>();
-        loop {
-            if GetMessageW(&mut msg, ptr::null_mut(), 0, 0) == 0 {
-                return;
-            }
-            TranslateMessage(&mut msg);
-            DispatchMessageW(&mut msg);
+            // Print some information to the console
+            println!("Display name = {}", name.to_str().unwrap());
+            println!("    Left: {}", monitor.rcWork.left);
+            println!("   Right: {}", monitor.rcWork.right);
+            println!("     Top: {}", monitor.rcWork.top);
+            println!("  Bottom: {}", monitor.rcWork.bottom);
         }
     }
 }
 
-fn encode(source: &str) -> Vec<u16> {
-    source.encode_utf16().chain(Some(0)).collect()
-}
+fn enumerate_monitors() -> Vec<MONITORINFOEXW> {
+    // Define the vector where we will store the result
+    let mut monitors = Vec::<MONITORINFOEXW>::new();
+    let userdata = &mut monitors as *mut _;
 
-unsafe fn register_wndclass(class_name: &[u16]) -> bool {
-    let mut winc = mem::zeroed::<WNDCLASSW>();
-    winc.style = CS_HREDRAW | CS_VREDRAW;
-    winc.lpfnWndProc = Some(win_proc);
-    winc.hIcon = LoadIconW(ptr::null_mut(), IDI_APPLICATION);
-    winc.hCursor = LoadCursorW(ptr::null_mut(), IDC_ARROW);
-    winc.hbrBackground = GetStockObject(WHITE_BRUSH as i32) as HBRUSH;
-    winc.lpszClassName = class_name.as_ptr();
-
-    RegisterClassW(&winc) > 0
-}
-unsafe fn create_window(class_name: &[u16]) -> HWND {
-    CreateWindowExW(
-        0, //dwExStyle: DWORD
-        class_name.as_ptr(), //lpClassName: LPCWSTR
-        encode("Hello, World!").as_ptr(), //lpWindowName: LPCWSTR
-        WS_OVERLAPPEDWINDOW, //dwStyle: DWORD
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, //X: int, Y: int, nWidth: int, nHeight: int
-        ptr::null_mut(), //hWndParent: HWND
-        ptr::null_mut(), //hMenu: HMENU
-        ptr::null_mut(), //hInstance: HINSTANCE
-        ptr::null_mut(), //lpParam: LPVOID
-    )
-}
-unsafe extern "system" fn win_proc(hwnd: HWND, msg: UINT, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
-    match msg {
-        WM_DESTROY => PostQuitMessage(0),
-        _ => return DefWindowProcW(hwnd, msg, w_param, l_param),
+    let result = unsafe {
+        EnumDisplayMonitors(
+            ptr::null_mut(),
+            ptr::null(),
+            Some(enumerate_monitors_callback),
+            userdata as LPARAM,
+        )
     };
-    0
+
+    if result != TRUE {
+        // Get the last error for the current thread.
+        // This is analogous to calling the Win32 API GetLastError.
+        panic!("Could not enumerate monitors: {}", Error::last_os_error());
+    }
+
+    monitors
+}
+unsafe extern "system" fn enumerate_monitors_callback(
+    monitor: HMONITOR,
+    _: HDC,
+    _: LPRECT,
+    userdata: LPARAM,
+) -> BOOL {
+    // Get the userdata where we will store the result
+    let monitors: &mut Vec<MONITORINFOEXW> = mem::transmute(userdata);
+
+    // Initialize the MONITORINFOEXW structure and get a pointer to it
+    let mut monitor_info: MONITORINFOEXW = mem::zeroed();
+    monitor_info.cbSize = mem::size_of::<MONITORINFOEXW>() as u32;
+    let monitor_info_ptr = <*mut _>::cast(&mut monitor_info);
+
+    // Call the GetMonitorInfoW win32 API
+    let result = GetMonitorInfoW(monitor, monitor_info_ptr);
+    if result == TRUE {
+        // Push the information we received to userdata
+        monitors.push(monitor_info);
+    }
+
+    TRUE
 }
